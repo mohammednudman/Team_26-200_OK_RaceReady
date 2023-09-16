f00f20319842f889903929d20f5ed5dcdee32b0c
Mohammed Nudman Raza Shaikh <mohammednudman@gmail.com>
2023-09-16 12:40:17 +0530

    Chat backend system added

--- server/config/mongoDbConfig.js
+++ server/config/mongoDbConfig.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.DATABASE_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true})
     } catch (err) {
         console.log(err)
     }
 }
 
-module.exports = connectDB
+module.exports = {connectDB, mongoose};
